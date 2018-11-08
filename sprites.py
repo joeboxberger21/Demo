ae6f52baa3784a3511f040896843715c8816525b
joeboxberger21 <44782547+joeboxberger21@users.noreply.github.com>
2018-11-08 14:11:09 -0800

    2nd Commit

--- sprites.py
+++ sprites.py
@@ -20,13 +20,20 @@ class Player(Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.center = (self.width / 2, self.height / 2)
+        self.rect.x = WIDTH / 2
+        self.rect.y = HEIGHT / 2
         self.vx = 0
         self.vy = 0
         self.speed = 5
+        self.falling = False
+        self.weapon = Weapon()
+        self.weapon.player = self
     
     def update(self):
+        self.weapon.update()
         self.vx = 0
         self.vy = 0
+        # self.gravity()
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
             self.vx = -self.speed
@@ -40,10 +47,26 @@ class Player(Sprite):
             # 1/square root 2 = ~0.7071, use this to lower the vx and vy if moving both at once, this number is found b/c of a^2 + b^2 = c^2
             self.vx *= 0.7071
             self.vy *= 0.7071
+        
+        if keys[pg.K_SPACE] and self.falling == False:
+            self.jump()
 
-        print('X: ' + str(self.rect.x) + ' Y: ' + str(self.rect.y))
         self.rect.x += self.vx
         self.rect.y += self.vy
+
+    def jump(self):
+        self.vy = -30
+
+    def gravity(self):
+        if self.rect.y < HEIGHT - self.height:
+            self.falling = True
+            self.vy += 3
+        elif self.rect.y >= HEIGHT:
+            self.falling = False
+            self.vy = 0
+            self.rect.y = HEIGHT - self.height
+
+
 
 class Enemy(Sprite):
     def __init__(self):
@@ -71,7 +94,6 @@ class Enemy(Sprite):
             #Create a multiplier from the distance between the player, 
             self.x = (self.follow.rect.x - self.rect.x) / self.c
             self.y = (self.follow.rect.y - self.rect.y) / self.c
-        print('c: ' + str(self.c))
 
         self.rect.x += self.x * self.speed
         self.rect.y += self.y * self.speed
@@ -79,20 +101,32 @@ class Enemy(Sprite):
 class Weapon(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.width = 60
-        self.height = 10
-        self.image = pg.Surface((self.height, self.height))
-        self.image.fill(BLUE)
+        self.width = 10
+        self.height = 60
+        self.image = pg.Surface((self.width, self.height)).convert_alpha()
+        self.rotated_image = self.image
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.center = (self.width / 2, self.height / 2)
         self.player = None
     
     def update(self):
-        mx, my = pg.mouse.get_pos()
+        self.mx, self.my = pg.mouse.get_pos()
         #TODO: Rotate based on mouse pos
-
         self.rect.x = self.player.rect.x + self.player.width / 2
         self.rect.y = self.player.rect.y + self.player.height / 2
+
+        self.cent_x, self.cent_y = self.player.rect.x + self.player.width / 2 , self.player.rect.y + self.player.height / 2
+
+        side1_length = math.sqrt((self.cent_x - self.mx) **2 + ((self.cent_y + self.player.height / 2) - self.my)**2)
+        side2_length = self.player.height / 2
+        side3_length = math.sqrt((self.cent_x - self.mx) **2 + (self.cent_y - self.my)**2)
+
+        deg_rotate = math.degrees(math.acos(((side2_length **2) + (side3_length **2) - (side1_length **2)) / (2 * side2_length * side3_length)))
+        print(deg_rotate)
+        if self.mx <= self.player.rect.x:
+            deg_rotate *= -1
+        self.rotated_image = pg.transform.rotate(self.image, deg_rotate)
 
 # class Projectile(Sprite):
 #     def __init__(self):
@@ -103,3 +137,14 @@ class Weapon(Sprite):
 #         self.image.fill(WHITE)
 #         self.rect = self.image.get_rect()
 #     def update(self):
+
+class Platform(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.width = 500
+        self.height = 20
+        self.image = pg.Surface((self.width, self.height))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH / 2
+        self.rect.y = HEIGHT - 100
